d1f68b2838ec7ebc6f14d9307f4a33289ed92eee
Samuel <samuelbsantosn@gmail.com>
2021-05-26 10:55:47 -0300

    Initial Commit

---
+++ land in sight/land_in_sightApp.swift
@@ -0,0 +1,17 @@
+//
+//  land_in_sightApp.swift
+//  land in sight
+//
+//  Created by Samuel Brasileiro on 26/05/21.
+//
+
+import SwiftUI
+
+@main
+struct land_in_sightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
