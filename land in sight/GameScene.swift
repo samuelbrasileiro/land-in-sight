69ec82760b9a60811b977797fed3e08c7f630426
Luis Pereira <lfbp@academy.cin.ufpe.br>
2021-06-01 11:57:32 -0300

    Player class created, move function working

--- land in sight/GameScene.swift
+++ land in sight/GameScene.swift
@@ -23,18 +23,15 @@ public enum ZPosition: Int {
 }
 
 public class GameScene: SKScene, GameDelegate {
-
-    var hasMoved: Bool = false
     
+    var hasMoved: Bool = false
+    var playerRect = Player()
     var trunks: [SKSpriteNode] = []
     
-    //var landBackground:SKTileMapNode!
-    
     public func updateGame() {
-        
     }
+    
     public func gameOver() {
-
     }
     
     override public func didMove(to view: SKView) {
@@ -43,86 +40,44 @@ public class GameScene: SKScene, GameDelegate {
         }
         
         self.hasMoved = true
-
-
-        guard let landBackground = childNode(withName: "Tile Map Node")
-                                       as? SKTileMapNode else {
-          fatalError("Background node not loaded")
-        }
-
-
-        print(landBackground.mapSize)
-        landBackground.setScale(1/75)
-
         
-//        let map = SKNode()
-//        addChild(map)
-//        map.xScale = 1/75
-//        map.yScale = 1/75
-//        let tileSet = SKTileSet(named: "Tile Set")!
-//        let tileSize = CGSize(width: 1024, height: 1024)
-//        let columns = 64
-//        let rows = 32
-//
-//        let deepWaterTiles = tileSet.tileGroups.first { $0.name == "Deep Water" }
-//        let shallowWaterTiles = tileSet.tileGroups.first { $0.name == "Shallow Water"}
-//        let sandTiles = tileSet.tileGroups.first { $0.name == "Sand"}
-//
-//        let bottomLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
-//        bottomLayer.fill(with: shallowWaterTiles)
-//        map.addChild(bottomLayer)
-//
-//        // create the noise map
-//        let noiseMap = makeNoiseMap(columns: columns, rows: rows)
-//
-//        // create our grass/water layer
-//        let topLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
-//
-//        // make SpriteKit do the work of placing specific tiles
-//        topLayer.enableAutomapping = true
-//
-//        // add the grass/water layer to our main map node
-//        map.addChild(topLayer)
-//
-//        for column in 0 ..< columns {
-//            for row in 0 ..< rows {
-//                let location = vector2(Int32(row), Int32(column))
-//                let terrainHeight = noiseMap.value(at: location)
-//                if row % 10 == 0{
-//
-////                    let coconut = SKSpriteNode(imageNamed: "coconut_tree")
-////                    coconut.setScale(1/75)
-////                    coconut.position.x += CGFloat(location.x)
-////                    addChild(coconut)
-//                }
-//                if terrainHeight < 0 {
-//                    topLayer.setTileGroup(sandTiles, forColumn: column, row: row)
-//                } else {
-//                    topLayer.setTileGroup(deepWaterTiles, forColumn: column, row: row)
-//                }
-//            }
-//        }
+        guard let landBackground = childNode(withName: "Tile Map Node")
+                as? SKTileMapNode else {
+            fatalError("Background node not loaded")
+        }
+        
+        playerRect.playerInit(assetName:"pirata 2", origin:CGPoint(x: 0, y: 0))
+        self.addChild(playerRect)
+        let camera = SKCameraNode()
+        camera.position = CGPoint(x: frame.midX, y: frame.midY)
+        camera.setScale(77)
+        self.camera = camera
+        self.addChild(camera)
+        let path:[CGPoint] = [CGPoint(x: 0*1024, y: 1*1024), CGPoint(x: 0*1024, y: 2*1024),CGPoint(x: 1*1024, y: 2*1024),CGPoint(x: 1*1024, y: 3*1024)]
+        movePlayer(path: path)
         
     }
-    func makeNoiseMap(columns: Int, rows: Int) -> GKNoiseMap {
-        let source = GKPerlinNoiseSource()
-        source.persistence = 0.9
-
-        let noise = GKNoise(source)
-        let size = vector2(1.0, 1.0)
-        let origin = vector2(0.0, 0.0)
-        let sampleCount = vector2(Int32(columns), Int32(rows))
-
-        return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
+    
+    func movePlayer(path:[CGPoint]?){
+        var actionSeqArr:[SKAction] = []
+        if let validPah = path{
+            for point in validPah{
+                let action = SKAction.move(to: point, duration: 2)
+                actionSeqArr.append(action)
+            }
+            let actionSeq = SKAction.sequence(actionSeqArr)
+            playerRect.run(actionSeq)
+        }
+        
     }
 }
 
 
 public class GameSceneLoader: ObservableObject{
     @Published public var scene: GameScene = GameScene()
-//    public init(){
-//        
-//    }
+    //    public init(){
+    //
+    //    }
     public init(/*environment: GameEnvironment*/){
         reset(/*environment*/)
     }
