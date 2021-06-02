4c817e2273f01d9554ea5a7c35ec90232f35855b
Samuel <samuelbsantosn@gmail.com>
2021-06-02 09:45:44 -0300

    Add lines to map

--- land in sight/GameScene.swift
+++ land in sight/GameScene.swift
@@ -22,59 +22,18 @@ public enum ZPosition: Int {
     case otherNodes
 }
 
-var visitedPoints: [CGPoint] = []
-
 class StopPoint{
     var challenge: String = ""
     var consequence: String = ""
     
-    var position: CGPoint
+    var index: Int
     
-    var lineToEnd: [CGPoint]?
     
-    init(position: CGPoint){
-        self.position = position
+    init(index: Int){
+        self.index = index
     }
     
-    //(5, 10)     (12, 13)
-    func getLinePositions(final: CGPoint) -> [CGPoint]{
-        var x = final.x - position.x
-        var y = final.y - position.y
-        
-        var movements: [CGPoint] = []
-        
-        while abs(x) > 0{
-            movements.append(CGPoint(x: (x >= 0 ? 1 : -1), y: 0))
-            x += (x >= 0 ? -1 : 1)
-        }
-        while abs(y) > 0{
-            movements.append(CGPoint(x: 0, y: (y >= 0 ? 1 : -1)))
-            y += (y >= 0 ? -1 : 1)
-        }
-        //[(1,0),(1,0),(1,0),(1,0),(1,0),(1,0),(1,0),(0,1),(0,1),(0,1)]
-        
-        
-        var start = position
-        
-        var positions: [CGPoint] = []
-
-            movements.shuffle()
-            positions = []
-                        
-            for movement in movements{
-                start.x += movement.x
-                start.y += movement.y
-                
-                positions.append(start)
-                if visitedPoints.contains(start){
-                    print("CONFLITOU ALGUMA VEZ NO \(start)")
-                    break
-                }
-                
-            }
-        
-        return positions
-    }
+    
 }
 
 public class GameScene: SKScene, GameDelegate {
@@ -105,32 +64,16 @@ public class GameScene: SKScene, GameDelegate {
         
         self.hasMoved = true
 
-
+    
         guard let landBackground = childNode(withName: "Tile Map Node")
                                        as? SKTileMapNode else {
           fatalError("Background node not loaded")
         }
 
-
         print(landBackground.mapSize)
         //landBackground.setScale(1/75)
 
         //x -31 31 y -15 15
-        let points = [StopPoint(position: CGPoint(x: -20, y: 10)),StopPoint(position: CGPoint(x: -10, y: -10)), StopPoint(position: CGPoint(x: 0, y: 5)), StopPoint(position: CGPoint(x: 20, y: 10))]
-        
-        var linesPositions: [(CGPoint, Bool)] = []
-        linesPositions.append((points[0].position, true))
-        for i in 0..<points.count-1{
-            
-            let lp = points[i].getLinePositions(final: points[i+1].position)
-            points[i].lineToEnd = lp
-            
-            linesPositions.append(contentsOf: lp.dropLast().map{($0, false)})
-            linesPositions.append((points[i].position, true))
-        }
-        
-        
-        printMap(positions: linesPositions)
         
         
         let camera = SKCameraNode()
@@ -141,23 +84,6 @@ public class GameScene: SKScene, GameDelegate {
         
     }
     
-    func printMap(positions: [(CGPoint, Bool)]){
-        if positions.isEmpty{
-            return
-        }
-        let position = positions.first!
-        
-        let rect = SKShapeNode(rect: CGRect(x: frame.midX , y: frame.midY, width: 1024, height: 1024))
-        rect.fillColor = position.1 ? .green : .black
-        
-        
-        rect.position = CGPoint(x:position.0.x * 1024, y: position.0.y * 1024)
-        print(position.0)
-        //rect.setScale(1/75)
-        self.addChild(rect)
-        
-        printMap(positions: [(CGPoint, Bool)](positions.dropFirst()))
-    }
     
 }
 
